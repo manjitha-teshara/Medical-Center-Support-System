a89f7801dfbf326c60543bcb7b010820ec2b7b27
unknown <pkashmari1996@gmail.com>
2019-05-30 08:52:48 +0530

    Patient records intable from db

--- myapp/frontend/src/app/users/admin/admin.component.ts
+++ myapp/frontend/src/app/users/admin/admin.component.ts
@@ -2,33 +2,26 @@ import { Component, OnInit } from '@angular/core';
 
 import 'rxjs/add/observable/of';
 import { MatTableDataSource } from '@angular/material/table';
-import {MatTabsModule} from '@angular/material/tabs';
+//import {MatTabsModule} from '@angular/material/tabs';
 import { MedicineService } from '../../shared/medicine.service';
 import { Medicine} from '../../shared/medicine.model';
-import { NgForm } from '@angular/forms';
-
+////import { NgForm } from '@angular/forms';
+import { PatientRecordsService } from 'src/app/shared/patientRecords/patient-records.service';
+//import { MatInputModule } from '@angular/material';
+import { from } from 'rxjs';
+import { PatientRecordClass } from 'src/app/shared/patientRecords/patient-record-class.model';
+//import {MatDialogModule} from '@angular/material/dialog';
 
 export class TabGroupDynamicHeightExample {}
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
+// export interface PeriodicElement {
+//   name:String;
+//   age: number;
+//   date: number;
+//   description: string;
+ 
+// }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
 
 
 @Component({
@@ -39,24 +32,47 @@ const ELEMENT_DATA: PeriodicElement[] = [
 
 
 export class AdminComponent implements OnInit {
-  // ngOnInit(): void {
-  //   throw new Error("Method not implemented.");
-  // }
+  // dataSource :any;
+  public dataSource = new MatTableDataSource<PatientRecordClass>();
+ 
+  constructor(private  patientRecordService:  PatientRecordsService) {}
+  displayedColumns: string[] = ['name','date','age','description','actions'];
+  // dataSource = new MatTableDataSource(ELEMENT_DATA);
 
-  displayedColumns: string[] = ['position','name','weight','symbol','actions'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  
+ 
+   ngOnInit() {
 
+    this.getPatientRecord();
+    //  this.patientRecordService.getPatientRecordList().subscribe((result)=>{    
+    //   this.dataSource  =  result.body;
+      
+  }
+  getPatientRecord(){
+    this.patientRecordService.getPatientRecordList().subscribe((res)=>{
+      this.dataSource.data = res as PatientRecordClass[];
+    })
+    //   this.dataSource = res;
+    //   console.log(this.dataSource);
+    // },error =>{
+    //   console.log(error);
+
+    // })
+
+  }
   applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
- 
-   ngOnInit() {
   
-  }
-}
- 
-  export class CardOverviewExample {}
+  // onEdit(){
+  //   this.dialog
+  // }
 
+}
+export class CardOverviewExample {}
+
+ 
+ 
 
 
   export class MedicineComponent implements OnInit{
